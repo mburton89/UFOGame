07b16b766c39574a89b864170659b137c1555300
DESKTOP-AVTI26A\burto <matthewwburton@gmail.com>
2018-12-02 20:58:29 -0600

    All the things

--- SideViewTest/Assets/Scripts/Enemy.cs
+++ SideViewTest/Assets/Scripts/Enemy.cs
@@ -7,17 +7,17 @@ public class Enemy : MonoBehaviour
     public void Init(Vector2 direction, float speed)
     {
         GetComponent<Rigidbody2D>().AddForce(direction * speed, ForceMode2D.Impulse);
-        Destroy(gameObject, 10f);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        print("Collision");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        print("Collision2D");
         EnemyDispenserController.Instance.HandleEnemyDestroyed(this);
     }
+
+	private void OnDestroy(){
+		if (EnemyDispenserController.Instance != null) 
+		{
+			EnemyDispenserController.Instance.HandleEnemyDestroyed(this);
+		}
+	}
 }
--- SideViewTest/Assets/Scripts/EnemyDispenserController.cs
+++ SideViewTest/Assets/Scripts/EnemyDispenserController.cs
@@ -7,8 +7,10 @@ public class EnemyDispenserController : MonoBehaviour
     public static EnemyDispenserController Instance;
 
     [SerializeField] private EnemyDispenser topLeftDispenser;
-    [SerializeField] private EnemyDispenser topRightDispenser;
-    [SerializeField] private EnemyDispenser bottomLeftDispenser;
+	[SerializeField] private EnemyDispenser topLeft2Dispenser;
+	[SerializeField] private EnemyDispenser topRightDispenser;
+	[SerializeField] private EnemyDispenser topRight2Dispenser;
+	[SerializeField] private EnemyDispenser bottomLeftDispenser;
     [SerializeField] private EnemyDispenser bottomRightDispenser;
 
     public float speed;
@@ -19,8 +21,10 @@ public class EnemyDispenserController : MonoBehaviour
     {
         Instance = this;
 
-        topLeftDispenser.Init(this);
-        topRightDispenser.Init(this);
+		topLeftDispenser.Init(this);
+		topLeft2Dispenser.Init(this);
+		topRightDispenser.Init(this);
+		topRight2Dispenser.Init(this);
         bottomLeftDispenser.Init(this);
         bottomRightDispenser.Init(this);
     }
@@ -40,7 +44,28 @@ public class EnemyDispenserController : MonoBehaviour
 
     void FireRandomSequence()
     {
-        FireFour();
+		var randomInt = Random.Range (0, 4);
+
+		if (randomInt == 0)
+		{
+			FireFour();
+		}
+		else if (randomInt == 1)
+		{
+			FireTwoLeft();
+		}
+		else if (randomInt == 2)
+		{
+			FireTwoRight();
+		}
+		else if (randomInt == 3)
+		{
+			FireMix1();
+		}
+		else if (randomInt == 4)
+		{
+			FireMix2();
+		}
     }
 
     public void FireFour()
@@ -50,6 +75,30 @@ public class EnemyDispenserController : MonoBehaviour
         activeEnemies.Add(bottomLeftDispenser.FireEnemy());
         activeEnemies.Add(bottomRightDispenser.FireEnemy());
     }
+
+	public void FireTwoLeft()
+	{
+		activeEnemies.Add(topLeft2Dispenser.FireEnemy());
+		activeEnemies.Add(bottomLeftDispenser.FireEnemy());
+	}
+
+	public void FireTwoRight()
+	{
+		activeEnemies.Add(topRight2Dispenser.FireEnemy());
+		activeEnemies.Add(bottomLeftDispenser.FireEnemy());
+	}
+
+	public void FireMix1()
+	{
+		activeEnemies.Add(topLeft2Dispenser.FireEnemy());
+		activeEnemies.Add(bottomRightDispenser.FireEnemy());
+	}
+
+	public void FireMix2()
+	{
+		activeEnemies.Add(topRight2Dispenser.FireEnemy());
+		activeEnemies.Add(bottomLeftDispenser.FireEnemy());
+	}
 
     public void HandleEnemyDestroyed(Enemy destroyedEnemy)
     {
